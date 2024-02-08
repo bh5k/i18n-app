9a25759ae80751c523150ac50fc7c2cc9dc26e7e
Bryan Hansen <bryankhansen@gmail.com>
2024-02-08 06:16:09 -0700

    checked in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import i18n from './i18n'; // Assuming you have your i18n configuration in i18n.js
 
 function App() {
+  const { t } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng); // Use the changeLanguage function from the i18n instance
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{t('Welcome to my app')}</h1>
+      <button onClick={() => changeLanguage('en')}>English</button>
+      <button onClick={() => changeLanguage('fr')}>French</button>
     </div>
   );
 }
